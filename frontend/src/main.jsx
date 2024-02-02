20ed87a36c83bb1e3cfc640badb1e88f9fe80544
HRDs-VERSE <hrds.verse@gmail.com>
2024-02-02 14:59:35 +0530

    unwnated imports remove

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { Provider } from 'react-redux';
